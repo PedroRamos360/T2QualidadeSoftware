335e6ea4e1c8c723f2ba06b2d3d088c3b3274cd2
Rodrigo Appelt <rappelt@inf.ufsm.br>
2024-05-16 11:27:50 -0300

    jmockit funciona

--- JMockit2/src/test/java/com/rodrigoappelt/qualidade/test/PaymentProcessorTest.java
+++ JMockit2/src/test/java/com/rodrigoappelt/qualidade/test/PaymentProcessorTest.java
@@ -1,18 +1,18 @@
 package com.rodrigoappelt.qualidade.test;
 
 import mockit.Expectations;
+import mockit.Verifications;
 import org.testng.annotations.Test;
 import mockit.Mocked;
 import static org.testng.Assert.assertEquals;
 
 public class PaymentProcessorTest {
 
-    @Mocked
-    private Calculator calculator;
+    @Mocked Calculator calculator;
 
     @Test
     public void testCalculateTotal() {
-        // Mock behavior
+        // cria o comportamento mock da funcao
         new Expectations() {{
             calculator.multiply(10.0, 3); result = 30.0;
         }};
@@ -20,6 +20,11 @@ public class PaymentProcessorTest {
         PaymentProcessor paymentProcessor = new PaymentProcessor(calculator);
         double total = paymentProcessor.calculateTotal(10.0, 3);
         assertEquals(total, 30.0);
+
+        // checa se a funcao foi chamada mesmo
+        new Verifications() {{
+            calculator.multiply(10.0, 3); times = 1;
+        }};
     }
 }
 
